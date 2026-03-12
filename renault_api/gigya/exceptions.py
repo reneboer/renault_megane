6edb84bc1a1a681fb080760f0656e6da6abfadea
reneboer <github@boerhome.nl>
2026-03-12 21:32:17 +0100

    Initial commit
    Version 0.1.0

---
+++ renault_api/gigya/exceptions.py
@@ -0,0 +1,24 @@
+"""Gigya exceptions."""
+
+from renault_api.exceptions import RenaultException
+
+
+class GigyaException(RenaultException):
+    """Base exception for Gigya errors."""
+
+    pass
+
+
+class GigyaResponseException(GigyaException):
+    """Gigya returned a parsable errors."""
+
+    def __init__(self, error_code: int, error_details: str | None):
+        """Initialise GigyaResponseException."""
+        self.error_code = error_code
+        self.error_details = error_details
+
+
+class InvalidCredentialsException(GigyaResponseException):
+    """Invalid loginID or password."""
+
+    pass
